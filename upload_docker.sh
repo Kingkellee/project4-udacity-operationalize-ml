5ef7ae5ae484386acb3774a51e4b07dad2e22137
kingkellee <kellyiyogun@gmail.com>
2022-07-30 00:57:44 +0100

    kubernetes

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="kingkellee/sklearn-api:sklearn-api-v1"
 
 # Step 2:  
 # Authenticate & tag
+docker login
+docker tag sklearn-api ${dockerpath}
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ${dockerpath}
