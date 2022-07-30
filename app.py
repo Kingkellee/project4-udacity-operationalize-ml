33ffcc50f026a6159b77b4c06f367144e37e2a92
Kingkellee <kingkellee@KingKellee.localdomain>
2022-07-30 11:32:57 +0100

    project complete

--- app.py
+++ app.py
@@ -64,7 +64,6 @@ def predict():
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
     LOG.info(f"Prediction: \n{prediction}")
-    print("Completed Predictions!!!")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
